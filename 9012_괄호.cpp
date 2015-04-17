31814ab764690ef342f2775392c2a33f70542a96
HubStew <atnacm@naver.com>
2015-04-17 10:04:49 +0900

    add 9012 Parenthesis
    accpet

---
+++ 9012_괄호.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+#define MAX_VPS 51
+
+void init(bool *arr)
+{
+	for (int i = 0; i < MAX_VPS; i++)
+		arr[i] = 0;
+}
+
+int main()
+{
+	int testCase;
+	cin >> testCase;
+
+	string ps;
+	bool isVPS[MAX_VPS];
+	while(testCase--)
+	{
+		cin >> ps;
+		init(isVPS);
+
+		for (int i = 0; i < ps.length(); i++)
+		{
+			if (ps[i] == '(' && isVPS[i] == 0 )
+			{
+			
+				for (int j = i+1; j < ps.length(); j++)
+				{
+					if (ps[j] == ')'&& isVPS[j] == 0)
+					{
+						isVPS[i] = 1;
+						isVPS[j] = 1;
+						break;
+					}
+				}
+			}
+			/*else if (ps[i] == ')' && isVPS[i] == 0)
+			{
+				
+				for (int j = i+1; j < ps.length(); j++)
+				{
+					if (ps[j] == '('&& isVPS[j] == 0)
+					{
+						isVPS[i] = 1;
+						isVPS[j] = 1;
+						break;
+					}
+				}
+			}*/
+
+			
+		}
+
+		bool result = true;
+		for (int i = 0; i < ps.length(); i++)
+		{
+			if (isVPS[i] == 0)
+			{
+				result = false;
+				break;
+			}
+			//cout << isVPS[i];
+		}
+		if (result == true)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+			//cout << endl;
+	}
+
+
+	return 0;
+}
